bed908bcb5959483bcd702fd478d8b4481467297
Tathagya <eveninghappy11@gmail.com>
2026-03-26 14:44:51 +0530

    start

---
+++ backend/versioned-notes/src/main/java/com/example/versionednotes/VersionedNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.versionednotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionedNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersionedNotesApplication.class, args);
+    }
+
+}
---
+++ backend/versioned-notes/src/test/java/com/example/versionednotes/VersionedNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.versionednotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionedNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
